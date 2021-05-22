d379a62c746ce9e5dca7a69e45db817221a87398
zatcham <37421850+zatcham@users.noreply.github.com>
2021-05-22 16:32:48 +0100

    Update to tscipherlib
    https://github.com/tomrow/tscipherlib/commit/b38b058b3db6cb4534a6e95eda4e2b0180b5d1a7
    
    Implements a new RNG mechanism, hopefully allowing for the library to be ported to other programming languages

--- tscipherlib.py
+++ tscipherlib.py
@@ -1,99 +1,113 @@
 import math
 import random
-
-
-def cencode(text, key):
+def cencode(text,key):
     output = []
     random.seed(key)
     for h in range(len(text)):
         i = h + 1
         output.append(
-            (ord(text[h]) + cscramble(i, key))
-            % 255)
+            (ord(text[h])+cscramble(i,key))
+            %255)
     return output
 
-
-def cencodeh(text, key):
+def cencodeh(text,key):
     output = ""
     random.seed(key)
     for h in range(len(text)):
         i = h + 1
-        chara = hex((ord(text[h]) + cscramble(i, key)) % 255)
+        chara = hex((ord(text[h])+cscramble(i,key))%255)
         chara = chara[2:]
-        while len(chara) < 2:
+        while len(chara)<2:
             chara = "0" + chara
         output = output + chara
     return output
 
-
-def cscramble(iterate, key):
+def cscramblem(iterate,key):
     interim = (iterate
-               + (key % 10) * iterate + math.floor(iterate / 3)
-               + iterate * 2
-               + math.floor(9 * math.sin(math.radians(iterate * 2)))
-               + random.randint(0, 200)
-               ) % 255
-    return interim + 255
+            +(key%10)*iterate+math.floor(iterate/3)
+            +iterate*2
+            +math.floor(9*math.sin(math.radians(iterate*2)))
+            +random.randint(0,200)
+           )%255
+    return interim+255
 
-
-def cdecode(array, key):
+def cscramble(iterate,key):
+    interim = iterate
+    interim += (key%10)*iterate
+    interim += math.floor(iterate/3)
+    interim += iterate*2
+    interim += math.floor(9*math.sin(math.radians(iterate*2)))  #add 9sin(iterate*2) with the decimal point chopped off
+    #interim += random.randint(0,200) #REMOVE!
+    for i in range(6):
+        interimb = math.sin(math.radians(key*2)) * (2**32) #make a very big number using key*2
+        interimb = math.floor(interimb) #make it int
+        interimb = interimb*3 ^ (iterate*7) #xor (iterate*7)
+        interimc = interimb >> 5
+        interimc = interimc << 5 #chop off a few bits
+        interimd = interimc << 3 #make another one
+        interime = interimb ^ interimc
+        interime += interimd
+        interim -= interime
+    interim = interim % 255
+    return interim+25
+                       
+def cdecode(array,key):
     output = ""
     random.seed(key)
     for h in range(len(array)):
         i = h + 1
-        maths = (array[h] - cscramble(i, key))
-        output = output + (chr(maths % 255))
+        maths =(array[h]-cscramble(i,key))
+        output = output + (chr(maths%255))
     return output
 
-
-def cdecodeh(stringin, key):
+def cdecodeh(stringin,key):
     stredit = stringin.lower()
-    if len(stringin) % 2 == 1:
+    if len(stringin)%2 == 1:
         stredit = "0" + stringin
-    hexdigits = {"0": 0,
-                 "1": 1,
-                 "2": 2,
-                 "3": 3,
-                 "4": 4,
-                 "5": 5,
-                 "6": 6,
-                 "7": 7,
-                 "8": 8,
-                 "9": 9,
-                 "a": 10,
-                 "b": 11,
-                 "c": 12,
-                 "d": 13,
-                 "e": 14,
-                 "f": 15}
-
-    array = []
-    for c in range(math.floor(len(stredit) / 2)):
-        ptr = c * 2
-        array.append((hexdigits[stredit[ptr]] * 16) + hexdigits[stredit[ptr + 1]])
+    hexdigits = {"0" : 0 ,
+                 "1" : 1 ,
+                 "2" : 2 ,
+                 "3" : 3 ,
+                 "4" : 4 ,
+                 "5" : 5 ,
+                 "6" : 6 ,
+                 "7" : 7 ,
+                 "8" : 8 ,
+                 "9" : 9 ,
+                 "a" : 10 ,
+                 "b" : 11 ,
+                 "c" : 12 ,
+                 "d" : 13 ,
+                 "e" : 14 ,
+                 "f" : 15}
+    
+    array = []                 
+    for c in range(math.floor(len(stredit)/2)):
+        ptr = c*2
+        array.append((hexdigits[stredit[ptr]]*16)+hexdigits[stredit[ptr+1]])
+        
 
     output = ""
     random.seed(key)
     for h in range(len(array)):
         i = h + 1
-        maths = (array[h] - cscramble(i, key))
-        output = output + (chr(maths % 255))
+        maths =(array[h]-cscramble(i,key))
+        output = output + (chr(maths%255))
     return output
 
 
-# random.seed(5)
-# for i in range(5):
+
+#random.seed(5)
+#for i in range(5):
 #    print(random.randint(0,10000))
+
+
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    print(cdecode(cencode("hello", 1), 1))
-    a = cencode(
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        1)
+
+    print(cdecode(cencode("hello", 1),1))
+    a=cencode("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 1)
     for j in range(len(a)):
-        print(" " * math.floor(a[j] / 2) + "#")
-    print(cdecode([95, 175, 180, 10, 112, 3, 180, 11, 177, 196, 223, 119], 100))
-
-
+        print(" "*math.floor(a[j]/2)+"#")
