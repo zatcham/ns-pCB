70f8ff20c5e949ca78c64f5aa5b86a7fdce4d8ef
zatcham <37421850+zatcham@users.noreply.github.com>
2021-06-07 18:24:22 +0100

    Resolved bug surrounding duplicate values in OTP generation
    The initial OTP generation was quite flawed, causing lots of duplicate values, both between and within OTP files.
    
    Possibly have written some of the worst code ever to attempt to resolve this issue but at least it works.
    
    + removed unnecessary imports (sys/os not needed, pandas used instead of csv)

--- nsCB_otpgen.py
+++ nsCB_otpgen.py
@@ -1,15 +1,12 @@
 ## ns-pCB (number station - project Cherry Blossom)
 ## OTP Generation module
 ## Developed by Zach Matcham (zatcham)
-## Version 0.3 | 13/4/21
+## Version 0.4 | 7/6/21
 
-import sys
 import time
-import csv
-import os
 from random import seed
 from random import randint
-from datetime import datetime 
+from datetime import datetime
 import pandas as pd
 
 # RNG
@@ -106,71 +103,216 @@ numbers = {
 }
 
 # Iterate for numbers
-def generateNum():
+def generateNum(withOutput):
     for key, value in numbers.items():
         seed(newSeed())
         if value == "0":
             numbers.update({key: ranGen()})
-        print(key, '->', numbers[key])
+        if withOutput:
+            print(key, '->', numbers[key])
 
-def generateLA():
+def generateLA(withOutput):
     for key, value in lower_alphabet.items():
         seed(newSeed())
         if value == "0":
             lower_alphabet.update({key: ranGen()})
-        print(key, '->', lower_alphabet[key])
+        if withOutput:
+            print(key, '->', lower_alphabet[key])
 
-def generateUA():
+def generateUA(withOutput):
     for key, value in upper_alphabet.items():
         seed(newSeed())
         if value == "0":
             upper_alphabet.update({key: ranGen()})
-        print(key, '->', upper_alphabet[key])
+        if withOutput:
+            print(key, '->', upper_alphabet[key])
 
 def checkForDuplicate():
-    i = 25
-    while i < 26:
-        if lower_alphabet[i] == upper_alphabet[i]:
-            print ("Duplicate!")
+    # i = 1
+    # while i < 26:
+    #     if lower_alphabet[i] == upper_alphabet[i]:
+    #         print ("Duplicate between lowercase and uppercase!")
+    #         print ("Exiting!")
+    #         exit()
+    #     elif lower_alphabet[i] == numbers[i]:
+    #         print ("Duplicate between lowercase and numbers!")
+    #         print ("Exiting!")
+    #         exit()
+    #     elif upper_alphabet[i] == numbers[i]:
+    #         print ("Duplicate between uppercase and numbers!")
+    #         print ("Exiting!")
+    #         exit()
+    #     else:
+    #         i += 1
+    # if lower_alphabet.values() in upper_alphabet.values():
+    #     print ("Duplicate between lowercase and uppercase!")
+    #     print ("Exiting!")
+    #     exit()
+    # elif lower_alphabet.values() in numbers.values():
+    #     print ("Duplicate between lowercase and numbers!")
+    #     print ("Exiting!")
+    #     exit()
+    # elif upper_alphabet.values() in numbers.values():
+    #     print ("Duplicate between uppercase and numbers!")
+    #     print ("Exiting!")
+    #     exit()
+    for key, value in lower_alphabet.items():
+        if value in upper_alphabet.values():
+            print("Duplicate between lowercase and uppercase!")
+            print (key, value)
+            print("Exiting!")
             exit()
-        elif lower_alphabet[i] == numbers[i]:
-            print ("Duplicate!")
+    for key_1 in lower_alphabet:
+        for key_2 in lower_alphabet:
+            if key_1 == key_2:
+                break
+            for item in lower_alphabet[key_1]:
+                if item in lower_alphabet[key_2]:
+                    print("Duplicate between lowercase!")
+                    print(key, value)
+                    print("Exiting!")
+                    exit()
+    for key, value in lower_alphabet.items():
+        if value in numbers.values():
+            print("Duplicate between lowercase and numbers!")
+            print (key, value)
+            print("Exiting!")
             exit()
-        elif upper_alphabet[i] == numbers[i]:
-            print ("Duplicate!")
+    for key, value in upper_alphabet.items():
+        if value in numbers.values():
+            print("Duplicate between uppercase and numbers!")
+            print (key, value)
+            print("Exiting!")
             exit()
-        else:
-            i += 1
+        if value in upper_alphabet.values():
+            print ("Duplicate within uppercase!")
+            print (key, value)
+            print("Exiting!")
+            exit()
+    for key, value in numbers.items():
+        if value in numbers.items():
+            print ("Duplicate within numbers!")
+            print (key, value)
+            print("Exiting!")
+            exit()
 
+# thx tom
+def checkSectionForDupe(origKey, origValue, killProgram, debugPrint, returnVal):
+    # toReturn = None
+    for iKey, iValue in lower_alphabet.items():
+        if (iKey != origKey) and (iValue == origValue):
+            if debugPrint:
+                print("Duplicate Found: \"" + str(iKey) + "\" and \"" + str(origKey) + "\" have a matching value: \n\"" + str(iValue) + "\", \"" + str(origValue) + "\"")
+            if killProgram:
+                print("A duplicate has been found, exiting")
+                exit()
+            if returnVal:
+                return True
+            # toReturn = iKey
+            break
+    for iKey, iValue in upper_alphabet.items():
+        if (iKey != origKey) and (iValue == origValue):
+            if debugPrint:
+                print("Duplicate Found: \"" + str(iKey) + "\" and \"" + str(origKey) + "\" have a matching value: \n\"" + str(iValue) + "\", \"" + str(origValue) + "\"")
+            if killProgram:
+                print("A duplicate has been found, exiting")
+                exit()
+            if returnVal:
+                return True
+            # toReturn = iKey
+            break
+    for iKey, iValue in numbers.items():
+        if (iKey != origKey) and (iValue == origValue):
+            if debugPrint:
+                print("Duplicate Found: \"" + str(iKey) + "\" and \"" + str(origKey) + "\" have a matching value: \n\"" + str(iValue) + "\", \"" + str(origValue) + "\"")
+            if killProgram:
+                print("A duplicate has been found, exiting")
+                exit()
+            if returnVal:
+                return True
+            # toReturn = iKey
+            break
+    return False
+
+def checkAllForDupes(killProgram, debugPrint, returnVal):
+    for origKey, origValue in lower_alphabet.items():
+        if checkSectionForDupe(origKey, origValue, killProgram, debugPrint, returnVal):
+            return True
+    for origKey, origValue in lower_alphabet.items():
+        if checkSectionForDupe(origKey, origValue, killProgram, debugPrint, returnVal):
+            return True
+    for origKey, origValue in lower_alphabet.items():
+        if checkSectionForDupe(origKey, origValue, killProgram, debugPrint, returnVal):
+            return True
+    return False
+
+def generateAll(withOutput):
+    i = 0
+    if i == 0:
+        print ("Generating Numbers")
+        generateNum(withOutput)
+        print ("Generating lowercase alphabet")
+        generateLA(withOutput)
+        print ("Generating uppercase alphabet")
+        generateUA(withOutput)
+        if checkAllForDupes(False, True, True):
+            while checkAllForDupes(False, True, True):
+                time.sleep(2)
+                resetDicts(withOutput)
+                print("----")
+                print("Regenerating due to duplicates")
+                print("Regenerating Numbers")
+                generateNum(withOutput)
+                print("Regenerating lowercase alphabet")
+                generateLA(withOutput)
+                print("Regenerating uppercase alphabet")
+                generateUA(withOutput)
+                i += 1
+            print("----")
+            print ("Amount of duplicates found: " + str(i))
+            print("----")
+            return True
+        else:
+            return True
+
+def resetDicts(withOutput):
+    for key, value in lower_alphabet.items():
+        lower_alphabet.update({key: "0"})
+        if withOutput:
+            print("changed key "+key+" to 0")
+    for key, value in upper_alphabet.items():
+        upper_alphabet.update({key: "0"})
+        if withOutput:
+            print("changed key " + key + " to 0")
+    for key, value in numbers.items():
+        numbers.update({key: "0"})
+        if withOutput:
+            print("changed key " + key + " to 0")
+    if withOutput:
+        print ("Reset all keys to 0")
 
 
 def touch(path):
     open(path, 'a').close()
 
 def otp_main():
-    print ("Generating Numbers")
-    generateNum()
-    print ("Generating lowercase alphabet")
-    generateLA()
-    print ("Generating uppercase alphabet")
-    generateUA()
+    if generateAll(False):
+        # CSV out
+        date_fn = str(generateDate())
+        print ("Exporting OTPs to CSV")
+        print ("File names will be: \n Numbers: OTPNum-" + date_fn + ".csv" 
+        "\n Lowercase: OTPLC-" + date_fn + ".csv" +
+        "\n Uppercase: OTPUC-" + date_fn + ".csv")
+        touch("otp/OTPNum-" + date_fn + ".csv")
+        touch("otp/OTPLC-" + date_fn + ".csv")
+        touch("otp/OTPUC-" + date_fn + ".csv")
 
-    # CSV out
-    date_fn = str(generateDate())
-    print ("Exporting OTPs to CSV")
-    print ("File names will be: \n Numbers: OTPNum-" + date_fn + ".csv" 
-    "\n Lowercase: OTPLC-" + date_fn + ".csv" + 
-    "\n Uppercase: OTPUC-" + date_fn + ".csv")
-    touch("otp/OTPNum-" + date_fn + ".csv")
-    touch("otp/OTPLC-" + date_fn + ".csv")
-    touch("otp/OTPUC-" + date_fn + ".csv")
-
-    nf = pd.DataFrame.from_dict(numbers, orient="index")
-    nf.to_csv("otp/OTPNum-" + date_fn + ".csv")
-    lcf = pd.DataFrame.from_dict(lower_alphabet, orient="index")
-    lcf.to_csv("otp/OTPLC-" + date_fn + ".csv")
-    ucf = pd.DataFrame.from_dict(upper_alphabet, orient="index")
-    ucf.to_csv("otp/OTPUC-" + date_fn + ".csv")
+        nf = pd.DataFrame.from_dict(numbers, orient="index")
+        nf.to_csv("otp/OTPNum-" + date_fn + ".csv")
+        lcf = pd.DataFrame.from_dict(lower_alphabet, orient="index")
+        lcf.to_csv("otp/OTPLC-" + date_fn + ".csv")
+        ucf = pd.DataFrame.from_dict(upper_alphabet, orient="index")
+        ucf.to_csv("otp/OTPUC-" + date_fn + ".csv")
 
 if __name__ == "__main__":
     otp_main()
